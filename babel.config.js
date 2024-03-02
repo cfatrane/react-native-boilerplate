11665d6e2ae9105f2326bf7a42e54a757f309694
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-03-02 18:21:13 +1100

    add abbsolute iport

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '*': './',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+        },
+      ],
+    ],
   };
 };
