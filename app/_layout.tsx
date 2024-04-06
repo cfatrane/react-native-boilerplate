2bdd2bb9fe0080e933f5ea49a16b113b606c29b1
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2024-04-07 01:14:24 +0200

    add expo router with tab nav

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router/stack";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
